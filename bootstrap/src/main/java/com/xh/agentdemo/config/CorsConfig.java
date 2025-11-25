1aac8c6f92a0c91f39185f360bd9aea47dc9bb6c
xuhan <axuhan@yeah.net>
2025-11-25 21:32:23 +0800

    修改跨域配置

--- bootstrap/src/main/java/com/xh/agentdemo/config/CorsConfig.java
+++ bootstrap/src/main/java/com/xh/agentdemo/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // 允许所有路径
-                        .allowedOrigins("*")  // 允许所有来源，由前置nginx代理执行跨域检查
+                        .allowedOriginPatterns("*")  // 允许所有来源，由前置nginx代理执行跨域检查
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的请求方法
                         .allowedHeaders("*") // 允许所有请求头
                         .allowCredentials(true); // 允许携带 Cookie
