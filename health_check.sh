2162347c7bc68fbdda466ff0e1800151725955a2
xuhan <axuhan@yeah.net>
2025-11-25 22:58:40 +0800

    fix health_check.sh

--- health_check.sh
+++ health_check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "waiting healthcheck..."
-#sleep 60
+sleep 60
 
 for times in {1..10}
 do
-  #sleep 10
+  sleep 10
   echo "healthcheck ${times} times..."
   healthCheckResult=$(curl -s -S http://localhost:8082/healthcheck)
   if [ "${healthCheckResult}" = "OK" ]; then
