704f68acec258ce746371160987b153d6e2f42a1
xuhan <axuhan@yeah.net>
2025-11-25 21:24:49 +0800

    修改跨域配置

--- bootstrap/src/main/java/com/xh/agentdemo/config/CorsConfig.java
+++ bootstrap/src/main/java/com/xh/agentdemo/config/CorsConfig.java
@@ -1,10 +1,11 @@
 package com.xh.agentdemo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration 使用nginx跨域配置代替此处配置
+@Configuration
 public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -12,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // 允许所有路径
-                        .allowedOrigins("http://localhost:5173")  // 允许的来源（可修改为前端地址）
+                        .allowedOrigins("*")  // 允许所有来源，由前置nginx代理执行跨域检查
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的请求方法
                         .allowedHeaders("*") // 允许所有请求头
                         .allowCredentials(true); // 允许携带 Cookie
